3e189f0e60425c0106605f22c8335ec0e50bd187
antoniocasas7 <antoniocasas7@gmail.com>
2019-09-29 12:34:43 +0200

    subida definitiva

--- WebApiUsers/Controllers/AccountController.cs
+++ WebApiUsers/Controllers/AccountController.cs
@@ -24,7 +24,7 @@ namespace WebApiUsers.Controllers
     public class AccountController : ApiController
     {
         //private const string LocalLoginProvider = "Local";
-        //private ApplicationUserManager _userManager;
+        //private ApplicationUserManager _userManager; prueba
 
         //public AccountController()
         //{
